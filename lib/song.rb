2a6d80e78f6a51c5d00fd9abfa1b468bf15f60af
Karim Alomari <karim.alomari@gmail.com>
2020-09-25 19:19:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,4 +39,19 @@ class Song
     @@all.detect{|song| song.name == name}
   end
   
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.create_from_filename(filename)
+    @@all << self.new_from_filename(filename)
+  end
+
+  def self.new_from_filename(name)
+    artist, song, genre_name = name.split(" - ")
+    fixed_name = genre_name.gsub('.mp3','')
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(fixed_name)
+    new(song, artist, genre)
+  end
 end
