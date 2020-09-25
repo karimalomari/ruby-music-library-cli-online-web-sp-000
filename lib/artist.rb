95996b61191ce3068f611e320f4fbfc428c69016
Karim Alomari <karim.alomari@gmail.com>
2020-09-25 19:10:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  extend Concerns::Findable
+  
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    new(name).tap do |artist|
+    artist.save
+    end
+  end
+  
+end
